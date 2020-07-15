96f6b70545250d6f30d712bc2ded0d09507abf1c
Wouter Buckens <wouter@epicteddy.com>
2020-07-15 09:56:32 +0200

    Make render variables optional

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ export { configure } from './options';
 export { parse } from './parser';
 export { Template } from './Template';
 
-export async function render(filename : string, data : Record<string, any>) : Promise<string> {
+export async function render(filename : string, data : Record<string, any> = {}) : Promise<string> {
 	let template = await Template.create(filename);
 	return template.render(data);
 }
